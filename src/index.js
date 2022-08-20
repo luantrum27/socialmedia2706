29c498f6e16575f67405dbd34301d8e79a7b8d95
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-08-20 15:42:38 +0700

    upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = process.env.PORT || 3000
+const db = require('./config/db')
+const cors = require('cors')
+
+// Connect to DataBase;
+db.connect()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('combined'))
+app.use(cors())
+
+// Routes init
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
